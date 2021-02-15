13c3c5beb155a9acbe3e25ce324bf4f56e41be98
Jonathan1306 <33557099+Jonathan1306@users.noreply.github.com>
2021-02-15 13:10:17 -0500

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(){
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+
+    public double distance(){ //get point's distance from origin (0,0)
+        return Math.sqrt((x*x) + (y*y)) ;
+    }
+
+    public double distance(int x, int y){
+        return Math.sqrt((this.x - x)*(this.x - x) + (this.y - y)*(this.y - y));
+    }
+    
+    public double distance(Point another){
+        return Math.sqrt((this.x - another.x)*(this.x - another.x) + (this.y - another.y)*(this.y - another.y));
+    }
+
+
+}
+
+
